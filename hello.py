6a896389d5b52b39dc25d7d835db8b596cabd752
stech0703 <dubey.shivam0703@gmail.com>
2021-03-14 17:48:03 +0530

    hello.py file

--- hello.py
+++ hello.py
@@ -1,46 +1,81 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, redirect, url_for, flash, session,request
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from sqlalchemy.orm import query
+from wtforms import StringField,TextAreaField, SubmitField, validators
 from wtforms.validators import DataRequired
+from wtforms import Form
+from flask_mail import Mail, Message
 
-class NameForm(FlaskForm):
-    name=StringField('What is your name ?',validators=[DataRequired()])
-    submit=SubmitField('Submit')
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='hard to guess'
+db = SQLAlchemy(app)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SECRET_KEY'] = 'warmachinerox'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_SUBJECT_PREFIX'] = '[QUERY]'
+app.config['FLASK_MAIL_SENDER'] = 'ADMIN <s.tech0703@gmail.com>'
+app.config['ADMIN'] = os.environ.get('ADMIN')
+
+
+mail = Mail(app)
 
 
 
+def send_mail(to,subject,template,**kwargs):
+    msg=Message(app.config['MAIL_SUBJECT_PREFIX']+subject,sender=app.config['MAIL_SENDER'],recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)    
+    
 
 
+class Query(db.Model):
+    __tablename__='contacts'
+    id = db.Column(db.Integer,primary_key =True)
+    name = db.Column(db.String(20))
+    email = db.Column(db.String(50))
+    subj = db.Column(db.String(100))
+    message = db.Column(db.String(200))
 
+    def __repr__(self):
+        return f"Query('{self.name}' , '{self.email}' , '{self.subj}' , '{self.message}')"
+        
+        
 
-bootstrap=Bootstrap(app)
-@app.route('/',methods=['Get','POST'])
+@app.route('/',methods=['GET' , 'POST'])
 def index():
-    name= None
-    form= NameForm()
-    if form.validate_on_submit():
-        name=form.name.data
-        form.name.data=''
-    return render_template('index.html',form=form,name=name)
+    if request.method == "POST":
+        Name = request.form.get("name")
+        Email = request.form.get("email")
+        Subj = request.form.get("subj")
+        Message = request.form.get("message")
+        msgg=Query(name=Name, email=Email, subj=Subj, message=Message)
+        db.session.add(msgg)
+        db.session.commit()
+        if app.config['ADMIN']:
+            send_mail(app.config['ADMIN'],'New Query','mail/query',name=Name,email=Email,subj=Subj,message=Message)
+        flash('Query submitted successfully... wait for reply from admin!!',category='alert')
+        return redirect(url_for('index'))
+    return render_template("contact.html")
 
-@app.route('/user/<name>')
-def user(name):
-    l=[]
-    for i in range(10):
-        l.append(i)
-    var = {
-         'name':name,
-         'list':l
-    }    
-    return render_template('user.html',context=var)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'),404
+        return render_template('404.html') , 404
+
 @app.errorhandler(500)
-def page_not_found(e):
-    return render_template('500.html'),500    
+def internal_server_error(e):
+        return render_template('500.html') , 500
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db,Query=Query)
